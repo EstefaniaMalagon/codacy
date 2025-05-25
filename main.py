4b1dbc4c0209ca8859cd50b5f4ded4d4d7f3414c
EstefaniaMalagon <estefania-malagonf@unilibre.edu.co>
2025-05-24 22:16:50 -0500

    Cambio para probar Codacy

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 def saludar(nombre): 
-    print(f"Hola {nombre}") 
+    print("Hola " + nombre) 
  
 def dividir(a, b): 
-    if b == 0:
-        print("Error: división por cero")
-        return None
     return a / b 
  
 saludar("Mundo") 
-resultado = dividir(10, 0)
-if resultado is not None:
-    print(resultado)
+print(dividir(10, 0))
